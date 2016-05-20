ca548c980b422e7d96b71337dc7207a441676691
Himanshu Tokekar <himanshu-tokekar@sigma-systems.com>
2016-05-20 15:47:28 -0400

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,12 +1,48 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'Go to Market',
+	completed: false
+}, {
+	id: 3,
+	description: 'Go home',
+	completed: true
+}];
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
+// get /todos
 
+app.get('/todos', function( req, res) {
+	res.json(todos);
+});
+// get /todos/2
+app.get('/todos/:id', function( req, res) {
+	var todoId = parseInt (req.params.id,10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+
+});
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+	res.status(404).send();
+};
+
+	res.send('Asking for todo with id of ' + req.params.id)
+});
 app.listen(PORT, function () {
 	console.log('Express Listening on port ' + PORT + '!');
 } );
