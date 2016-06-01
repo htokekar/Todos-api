0daec8f3064e855a92e12f3b53cd84c4ac9ef108
Himanshu Tokekar <himanshu-tokekar@sigma-systems.com>
2016-06-01 11:19:02 -0400

    new
    new updates

--- server.js
+++ server.js
@@ -1,33 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-<<<<<<< HEAD
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Feed the cat',
-=======
-	description: 'Go to Market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Go home',
->>>>>>> refs/remotes/origin/master
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-<<<<<<< HEAD
 // GET /todos
 app.get('/todos', function (req, res) {
 	res.json(todos);
@@ -36,24 +21,7 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-=======
-// get /todos
-
-app.get('/todos', function( req, res) {
-	res.json(todos);
-});
-// get /todos/2
-app.get('/todos/:id', function( req, res) {
-	var todoId = parseInt (req.params.id,10);
->>>>>>> refs/remotes/origin/master
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-<<<<<<< HEAD
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -62,22 +30,24 @@ app.get('/todos/:id', function( req, res) {
 	}
 });
 
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	// add id field
+	body.id = todoNextId++;
+
+	// push body into array
+	todos.push(body);
+	
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
-=======
-
-});
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-	res.status(404).send();
-};
-
-	res.send('Asking for todo with id of ' + req.params.id)
-});
-app.listen(PORT, function () {
-	console.log('Express Listening on port ' + PORT + '!');
-} );
->>>>>>> refs/remotes/origin/master
