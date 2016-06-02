aa93746e9775231b58bbe7c6a823bd8e7a11a10c
htokekar <himanshu.tokekar@sigma-systems.com>
2016-06-02 08:39:35 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,48 +1,64 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to Market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Go home',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// get /todos
-
-app.get('/todos', function( req, res) {
+// GET /todos
+app.get('/todos', function (req, res) {
 	res.json(todos);
 });
-// get /todos/2
-app.get('/todos/:id', function( req, res) {
-	var todoId = parseInt (req.params.id,10);
-	var matchedTodo;
 
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-});
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
-	res.status(404).send();
-};
-
-	res.send('Asking for todo with id of ' + req.params.id)
+		res.status(404).send();
+	}
 });
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();	
+	body.id = todoNextId++;
+
+	todos.push(body);
+	
+	res.json(body);
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 app.listen(PORT, function () {
-	console.log('Express Listening on port ' + PORT + '!');
-} );
+	console.log('Express listening on port ' + PORT + '!');
+});
