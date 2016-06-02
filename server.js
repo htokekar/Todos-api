1ff48c08949444172778527ded27d0dcbdca9106
Himanshu Tokekar <himanshu-tokekar@sigma-systems.com>
2016-06-02 14:16:45 -0400

    updated the correct data

--- server.js
+++ server.js
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var bodyParser = require('body-parser');
-var _ = require('underscore');
-
-var app = express();
-var PORT = process.env.PORT || 3000;
-var todos = [];
-var todoNextId = 1;
-
-app.use(bodyParser.json());
-
-app.get('/', function (req, res) {
-	res.send('Todo API Root');
-});
-
-// GET /todos
-app.get('/todos', function (req, res) {
-	res.json(todos);
-});
-
-// GET /todos/:id
-app.get('/todos/:id', function (req, res) {
-	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
-
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
-});
-
-// POST /todos
-app.post('/todos', function (req, res) {
-	var body = req.body;
-
-	// add id field
-	body.id = todoNextId++;
-
-	// push body into array
-	todos.push(body);
-	
-	res.json(body);
-});
-
-
-app.listen(PORT, function () {
-	console.log('Express listening on port ' + PORT + '!');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
@@ -131,4 +62,4 @@ app.delete('/todos/:id', function (req, res) {
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
->>>>>>> origin/master
+
