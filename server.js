ae7b1fc67d508639fce019a2e9c3719ed526b551
Himanshu Tokekar <himanshu-tokekar@sigma-systems.com>
2016-06-08 13:12:41 -0400

    add PUT todos

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//<<<<<<< HEAD
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
@@ -14,26 +13,9 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos?completed=true
+// GET /todos
 app.get('/todos', function (req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
-
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {completed: true});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed: false});
-	}
-
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _filter(filteredTodos, function (todo) {
-			return todo.indexOf(queryParams.q) > -1;
-		});
-	}
-
-
-
-	res.json(filteredTodos);
+	res.json(todos);
 });
 
 // GET /todos/:id
@@ -125,73 +107,3 @@ app.listen(PORT, function () {
 
 
 
-=======
-var express = require('express');
-var bodyParser = require('body-parser');
-var _ = require('underscore');
-
-var app = express();
-var PORT = process.env.PORT || 3000;
-var todos = [];
-var todoNextId = 1;
-
-app.use(bodyParser.json());
-
-app.get('/', function (req, res) {
-	res.send('Todo API Root');
-});
-
-// GET /todos
-app.get('/todos', function (req, res) {
-	res.json(todos);
-});
-
-// GET /todos/:id
-app.get('/todos/:id', function (req, res) {
-	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
-
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
-});
-
-// POST /todos
-app.post('/todos', function (req, res) {
-	var body = _.pick(req.body, 'description', 'completed');
-
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
-
-	body.description = body.description.trim();	
-	body.id = todoNextId++;
-
-	todos.push(body);
-	
-	res.json(body);
-});
-
-// DELETE /todos/:id
-app.delete('/todos/:id', function (req, res) {
-	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
-
-	if (!matchedTodo) {
-		res.status(404).json({"error": "no todo found with that id"});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
-});
-
-app.listen(PORT, function () {
-	console.log('Express listening on port ' + PORT + '!');
-});
-// <<<<<<< HEAD
-
-// =======
-// >>>>>>> refs/remotes/origin/heroku/master
->>>>>>> origin/master
